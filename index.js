f91d5f952b8563082a70d0db006a2074af5d193e
Daniel Padrino <danswarrior1@gmail.com>
2022-09-16 10:47:38 -0300

    balances

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-// const { Spot } = require("@danswar/binance-connector-node");
+const { Spot } = require("@danswar/binance-connector-node");
 
 exports.handler = async () => {
-  // const client = new Spot(process.env.APP_KEY, process.env.APP_SECRET);
+  const client = new Spot(process.env.APP_KEY, process.env.APP_SECRET);
+
+  const { data: spot } = await client.userAsset();
+  const { data: funding } = await client.fundingWallet();
+  const {
+    data: flexibleProducts
+  } = await client.savingsFlexibleProductPosition();
+  console.log({ spot, funding, flexibleProducts });
   const response = "hello";
   return response;
 };
